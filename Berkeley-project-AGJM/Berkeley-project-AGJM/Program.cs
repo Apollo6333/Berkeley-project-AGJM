3da52d2b8bb6866de7f12392a70ad24838102fe3
GregoriPintarelli <gregorip@furb.br>
2025-08-28 09:55:03 -0300

    Initial Commit

---
+++ Berkeley-project-AGJM/Berkeley-project-AGJM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Berkeley_project_AGJM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Uso: dotnet run <numeroProcessosIniciais> <portInicial> <portFinal>");
+                return;
+            }
+
+            int numeroProcessosInicias = int.Parse(args[0]);
+            int portInicial = int.Parse(args[1]);
+            int portFinal = int.Parse(args[2]);
+
+            if (portInicial >= portFinal)
+            {
+                Console.WriteLine("<portInicial> precisa ser menor que <portFinal>");
+                return;
+            }
+            if (portInicial <= 0)
+            {
+                Console.WriteLine("<portInicial> precisa ser maior que 0");
+                return;
+            }
+            if (portFinal <= 0)
+            {
+                Console.WriteLine("<portFinal> precisa ser maior que 0");
+                return;
+            }
+
+
+        }
+    }
+}
